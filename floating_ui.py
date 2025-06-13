79cb6158dae5564174f45036dfcd795b9019571e
tristantheguy <92645917+tristantheguy@users.noreply.github.com>
2025-06-13 01:27:54 -0400

    Make floating window movable

--- floating_ui.py
+++ floating_ui.py
@@ -13,10 +13,31 @@ class FloatingWindow(QtWidgets.QWidget):
         )
         self.resize(250, 120)
 
+        self._drag_pos = None
+
         layout = QtWidgets.QVBoxLayout(self)
         self.label = QtWidgets.QLabel("", self)
         self.label.setWordWrap(True)
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
         layout.addWidget(self.label)
+
+        # Show an initial friendly message
+        self.display_message("👋 I'm your assistant! (Drag me around)")
 
     def display_message(self, text):
         self.label.setText(text)
+
+    # -- Drag Support -----------------------------------------------------
+    def mousePressEvent(self, event):
+        if event.button() == QtCore.Qt.LeftButton:
+            self._drag_pos = event.globalPos() - self.frameGeometry().topLeft()
+            event.accept()
+
+    def mouseMoveEvent(self, event):
+        if event.buttons() & QtCore.Qt.LeftButton and self._drag_pos:
+            self.move(event.globalPos() - self._drag_pos)
+            event.accept()
+
+    def mouseReleaseEvent(self, event):
+        self._drag_pos = None
+        event.accept()
